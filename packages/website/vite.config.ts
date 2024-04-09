08cd05f582b9c8684eea8862784fb791808c6f09
Olivier Bossel <olivier.bossel@gmail.com>
2024-04-09 14:16:37 +0200

    up

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,55 +1,13 @@
 import browserslist from 'browserslist';
-import { browserslistToTargets } from 'lightningcss';
+import { browserslistToTargets, composeVisitors } from 'lightningcss';
 
-const colors = {};
+import __sugarcss from '@lotsof/sugarcss';
 
 export default {
   css: {
     transformer: 'lightningcss',
     lightningcss: {
-      visitor: {
-        Declaration: {
-          custom(v) {
-            if (v.name.match(/^--s-color-/)) {
-              const name = v.name.replace(/^--s-color-/, '');
-              colors[name] = v.value[0]?.value ?? v.value[0];
-            }
-            console.log(v);
-          },
-        },
-        Property: {
-          //   unknown(v) {
-          //     console.log(v);
-          //   },
-        },
-        Rule: {
-          unknown(v) {
-            console.log(JSON.stringify(v, null, 2));
-            console.log(v.toString());
-            if (v.property === 'unparsed') {
-              // Handle unparsed value, e.g. `var(--w)`
-            } else {
-              console.log(v);
-              // Handle parsed value, e.g. `12px`
-            }
-          },
-        },
-        Function: {
-          's-color': (value: any) => {
-            const arg1 = value.arguments[0];
-            switch (arg1.type) {
-              case 'token':
-                return {
-                  raw: `var(--s-color-${arg1.value.value})`,
-                };
-                break;
-            }
-            if (!colors[value]) {
-            }
-            return { raw: 'rgb(253, 0, 0)' };
-          },
-        },
-      },
+      visitor: composeVisitors([__sugarcss()]),
       targets: browserslistToTargets(browserslist('>= 0.25%')),
     },
   },
