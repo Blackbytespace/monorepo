7299f6885e2adedd9846f325785dbd0885cddfac
Olivier Bossel <olivier.bossel@gmail.com>
2024-04-11 17:24:39 +0200

    up

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,16 +1,11 @@
-import browserslist from 'browserslist';
-import { browserslistToTargets, composeVisitors } from 'lightningcss';
-
-import __sugarcss from '@lotsof/sugarcss';
+import { sugarize } from '@lotsof/sugarcss';
 
 export default {
   css: {
     transformer: 'lightningcss',
-    lightningcss: {
+    lightningcss: sugarize({
       minify: false,
-      visitor: composeVisitors([__sugarcss()]),
-      targets: browserslistToTargets(browserslist('>= 0.25%')),
-    },
+    }),
   },
   build: {
     cssMinify: false,
