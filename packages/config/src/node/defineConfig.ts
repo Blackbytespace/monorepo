541587f19752eaa3adbc49090ab31e986d046e65
Olivier Bossel <olivier.bossel@gmail.com>
2024-05-20 13:58:35 +0200

    up

--- packages/config/src/node/defineConfig.ts
+++
@@ -1,7 +0,0 @@
-import __config from './config.js';
-import { __deepMerge } from '@lotsof/sugar/object';
-
-export default function defineConfig(config: any): any {
-  process.lotsofConfig = __deepMerge(process.lotsofConfig ?? {}, config);
-  return process.lotsofConfig;
-}
--- packages/config/src/node/exports.ts
+++
@@ -1,5 +0,0 @@
-import __getConfig from './getConfig.js';
-import __defineConfig from './defineConfig.js';
-import __loadConfig from './loadConfig.js';
-
-export { __getConfig, __defineConfig, __loadConfig };
