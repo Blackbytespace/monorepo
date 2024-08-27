9adf73d7859ecb088ac41332a6816204d18e417c
Olivier Bossel <ob@buzzbrothers.ch>
2024-08-27 10:44:29 +0200

    add lerna

--- remove.sh
+++
@@ -1,5 +0,0 @@
-mv packages/litElement packages/litElement1
-git submodule deinit packages/litElement
-git rm --cached packages/litElement
-mv packages/litElement1 packages/litElement
-git add packages/litElement
